bef1652e08d44df21cef030502dc302e33be8937
Lucas Barros da Silva <lucas.silvaa1307@gmail.com>
2024-10-07 21:46:48 -0300

    primeiro commit

---
+++ app/src/main/java/github/comsrtummnuss/kotlin_android_crypto_monitor/MainActivity.kt
@@ -0,0 +1,32 @@
+package github.comsrtummnuss.kotlin_android_crypto_monitor
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import github.comsrtummnuss.kotlin_android_crypto_monitor.ui.theme.KotlinandroidcryptomonitorTheme
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Configurando a toolbar
+        val toolbarMain: Toolbar = findViewById(R.id.toolbar_main)
+        configureToolbar(toolbarMain)
+    }
+    private fun configureToolbar(toolbar: Toolbar) {
+        setSupportActionBar(toolbar)
+        toolbar.setTitleTextColor(getColor(R.color.white))
+        supportActionBar?.title = getText(R.string.app_title)
+        supportActionBar?.setBackgroundDrawable(getDrawable(R.color.primary))
+    }
+}
